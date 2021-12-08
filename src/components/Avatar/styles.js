69eb12a73f29055c75b0f7396a843d48795e08c7
Ramon <ramon1234rxm@gmail.com>
2021-12-08 16:23:09 -0300

    feat: make avatar component

---
+++ src/components/Avatar/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import Img from "gatsby-image"
+
+
+export const AvatarWrapper = styled(Img)`
+  border-radius: 50px;
+  height: 3.75rem;
+  margin: auto;
+  width: 3.75rem;
+`
