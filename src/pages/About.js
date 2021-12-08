46b67c11f9ebdb8d88467afe1a196d7ff2d916bf
Ramon <ramon1234rxm@gmail.com>
2021-12-08 09:36:13 -0300

    feat: make a about page

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+
+const AboutPage = () => (
+  <Layout>
+    <Seo title="About" />
+    <h1>About Page</h1>
+  </Layout>
+)
+
+export default AboutPage
