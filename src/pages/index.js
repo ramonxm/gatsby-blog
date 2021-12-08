11e88485157a41a7e5f7e64d599fbdc2af15dc14
Ramon <ramon1234rxm@gmail.com>
2021-12-08 09:36:29 -0300

    feat: update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
+import { Link } from "gatsby"
 import * as React from "react"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>ramon.dev</h1>
+    <ul>
+      <li>
+        <Link to='/'>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to='/about'>
+          About
+        </Link>
+      </li>
+    </ul>
   </Layout>
 )
 
