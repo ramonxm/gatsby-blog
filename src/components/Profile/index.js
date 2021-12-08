342be2a02e7039a74589f74ad6150be885e7ced7
Ramon <ramon1234rxm@gmail.com>
2021-12-08 09:35:48 -0300

    feat: make a profile component and query data

---
+++ src/components/Profile/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, description, position },
+    },
+  } = useStaticQuery(
+    graphql`
+      query MySiteMetadata {
+        site {
+          siteMetadata {
+            title
+            description
+            position
+          }
+        }
+      }
+    `
+  )
+  
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default Profile
