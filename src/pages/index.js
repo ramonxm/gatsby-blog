c6f5bb0016a08d9468f1e46b4ced1877f18f3ca2
Ramon <ramon1234rxm@gmail.com>
2021-12-08 16:25:42 -0300

    feat: update pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,20 @@
-import { Link } from "gatsby"
 import * as React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>ramon.dev</h1>
-    <ul>
-      <li>
-        <Link to='/'>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to='/about'>
-          About
-        </Link>
-      </li>
-    </ul>
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
