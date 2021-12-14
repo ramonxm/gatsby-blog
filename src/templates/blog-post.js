9f43124dfe7ade9025db073bac11d773862e12ce
Ramon <ramon1234rxm@gmail.com>
2021-12-14 09:16:14 -0300

    feat: make a templates to posts

---
+++ src/templates/blog-post.js
@@ -0,0 +1,46 @@
+
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from '../components/Layout'
+import Seo from '../components/seo'
+import * as S from '../components/Post/styles'
+
+const BlogPost = ({ data }) => {
+  const post = data.markdownRemark
+
+  return (
+    <Layout>
+      <Seo title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>
+          {post.frontmatter.title}
+        </S.PostTitle>
+        <S.PostDescription>
+          {post.frontmatter.description}
+        </S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+      }
+      html
+      timeToRead
+    }
+  }
+`
+
+export default BlogPost
